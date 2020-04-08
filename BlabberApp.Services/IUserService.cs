0a6fc7b8625001e14de1e363790f427960c575e9
Aubrey Sparrow <aubreysparrow@mail.weber.edu>
2020-04-07 21:24:15 -0600

    added user services

---
+++ BlabberApp.Services/IUserService.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using BlabberApp.Domain;
+
+namespace BlabberApp.Services
+{
+    public interface IUserService
+    {
+        IEnumerable GetAllUsers();
+        void AddNewUser(string email);
+        User CreateUser(string email);
+        User GetUser(string email);
+    }
+}
---
+++ BlabberApp.Services/UserService.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using BlabberApp.DataStore;
+using BlabberApp.Domain;
+
+namespace BlabberApp.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserAdapter userAdapter;
+
+        public UserService(UserAdapter adapter)
+        {
+            userAdapter = adapter;
+        }
+
+        public IEnumerable GetAll()
+        {
+            return userAdapter.GetAll();
+        }
+
+        public void AddNewUser(string email)
+        {
+            try
+            {
+                userAdapter.Add(CreateUser(email));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw new Exception(e.ToString());
+            }
+        }
+
+        public User CreateUser(string email)
+        {
+            return new User(email);
+        }
+
+        public User GetUser(string email)
+        {
+            return userAdapter.GetByEmail(email);
+        } 
+    }
+}
